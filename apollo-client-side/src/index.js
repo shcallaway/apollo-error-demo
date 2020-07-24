c44b9ed56cd5f53929c2d23de83d121888a8931f
Sherwood Callaway <shcallaway@gmail.com>
2020-07-23 21:54:12 -0400

    Try adding apollo-link-error
    We're trying to get a networkError to throw an exception in the console
    when errorPolicy is none. (This would emulate what we think is happening
    in dashboard currently.)

--- apollo-client-side/src/index.js
+++ apollo-client-side/src/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 
-import ApolloClient from "apollo-client";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
 import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from "apollo-link-error";
 import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: "http://localhost:4000"
 });
 
+const onErrorLink = onError(({ graphQLErrors, networkError }) => {});
+
 const client = new ApolloClient({
-  link,
+  link: ApolloLink.from([onErrorLink, httpLink]),
   // defaultOptions: {
   //   errorPolicy: "ignore"
   // },
